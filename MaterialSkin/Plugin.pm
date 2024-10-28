8507c8449162e4f58ac89db7bf56cc98a1bd8a85
darrell-k <darrell@darrell.org.uk>
2024-10-28 02:32:26 +0000

    Advanced Seach for Works

--- MaterialSkin/Plugin.pm
+++ MaterialSkin/Plugin.pm
@@ -1262,12 +1262,12 @@ sub _cliCommand {
             $params->{'action'} = 'saveLibraryView';
             $params->{'saveSearch'} = $saveLib;
         }
-        my ($tracks, $albums, $works) = Plugins::MaterialSkin::Search::advancedSearch($request->client(), $params);
+        my ($tracks, $albums, $works) = Plugins::MaterialSkin::Search::advancedSearch($request->client(), $params);;
 
         if ($saveLib) {
             Slim::Control::Request::notifyFromArray(undef, ['material-skin', 'notification', 'internal', 'vlib']);
         } else {
-            if (blessed($tracks)) {
+           if (blessed($tracks)) {
                 $tracks = $tracks->slice(0, $MAX_ADV_SEARCH_RESULTS);
                 my $count = 0;
                 while (my $track = $tracks->next) {
@@ -1335,13 +1335,14 @@ sub _cliCommand {
             if (blessed($works)) {
                 $works = $works->slice(0, $MAX_ADV_SEARCH_RESULTS);
                 my $count = 0;
-                #while (my $work = $works->next) {
-                #    $request->addResultLoop('works_loop', $count, 'work', $work->title);
-                #    $request->addResultLoop('works_loop', $count, 'work_id', $work->id);
-                #    $request->addResultLoop('works_loop', $count, 'composer_id', $work->composer);
-                #    $count++;
-                #    main::idleStreams() unless $count % 5;
-                #}
+                while (my $work = $works->next) {
+                    $request->addResultLoop('works_loop', $count, 'work', $work->title);
+                    $request->addResultLoop('works_loop', $count, 'work_id', $work->id);
+                    $request->addResultLoop('works_loop', $count, 'composer_id', $work->get_column('composer'));
+                    $request->addResultLoop('works_loop', $count, 'composer', $work->composer->get_column('name'));
+                    $count++;
+                    main::idleStreams() unless $count % 5;
+                }
             }
         }
         $request->setStatusDone();
@@ -2502,12 +2503,13 @@ use Slim::Web::Pages::Search;
 sub advancedSearch {
     my ($client, $params) = @_;
 
-    $params->{'searchType'} ||= 'Album';
-
     my @versionParts = split /\./, $::VERSION;
     if ($versionParts[0]>=9) {
-        $params->{'searchType'} ||= 'Work';
+        $params->{'searchType'} ||= 'AlbumWork';
+    } else{
+        $params->{'searchType'} ||= 'Album';
     }
+    
     return Slim::Web::Pages::Search::parseAdvancedSearchParams($client, $params);
 }
 
